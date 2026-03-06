053eb5dc1fe6146bf3c1bbe5bcc1a0e2568dc92e
DesarrolloNavitek <joserivera@navitek.com.mx>
2026-03-06 11:22:30 -0600

    Add files via upload

---
+++ spActualizarMinimosInventario_v1.sql
@@ -0,0 +1,94 @@
+SET NOCOUNT ON;
+--EXEC spActualizarMinimosInventario 45
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE NAME ='spActualizarMinimosInventario')
+DROP PROC dbo.spActualizarMinimosInventario
+GO
+CREATE PROCEDURE [dbo].[spActualizarMinimosInventario]
+@FDemandaProm		INT 
+AS
+BEGIN
+   DECLARE 
+	@FechaA				DATE = DATEADD(DAY, -1, GETDATE()),
+    @FechaD				DATE
+
+
+	SET @FechaD = DATEADD(MONTH, -3, @FechaA)
+
+	IF COALESCE(@FDemandaProm,0) = 0 
+	SET @FDemandaProm = 15
+
+
+    ;WITH UniversoArticulos AS (SELECT 
+        vt.Articulo, 
+        vt.Almacen,
+        SUM(CASE WHEN vt.Mov IN ('Factura','Factura Com.Ext40') AND vt.Estatus = 'CONCLUIDO' THEN vt.Cantidad ELSE 0 END) AS Demanda90,
+        SUM(CASE WHEN mt.Clave = 'VTAS.P' AND mt.SubClave = 'VTAS.PNVK' AND vt.Mov <> 'Cotizacion' AND vt.Estatus = 'PENDIENTE' AND Situacion = 'Autorización de Pedido' AND mt.Mov NOT LIKE 'PS%'
+				THEN CASE WHEN vt.Cantidad = VT.CantidadPendiente THEN vt.Cantidad ELSE vt.CantidadPendiente END 
+				ELSE 0 END) AS Pedido
+	FROM VentaTCalc vt
+	JOIN MovTipo	mt	ON vt.Mov=mt.Mov AND Modulo = 'VTAS'
+    WHERE vt.Almacen IN ('10','15') 
+      AND vt.FechaEmision BETWEEN @FechaD AND @FechaA
+    GROUP BY vt.Articulo, vt.Almacen
+	),Consumos AS (
+			SELECT	a.Articulo,
+					a.Almacen,
+					Demanda90, 
+					Pedido
+			FROM UniversoArticulos		a
+			LEFT JOIN ArtDisponible		b on a.Articulo=b.Articulo and a.Almacen=b.Almacen
+			WHERE Demanda90 > 0 
+			UNION 
+			ALL
+			SELECT	a.articulo,
+					a.Almacen,
+					Demanda90, 
+					Pedido
+			FROM UniversoArticulos		a
+			LEFT JOIN ArtDisponible			b on a.Articulo=b.Articulo and a.Almacen=b.Almacen
+			WHERE Pedido > 0 
+			GROUP BY  
+			a.articulo,
+			a.Almacen,	Demanda90, 
+			Pedido
+	), Totales AS (
+	SELECT  a.Almacen,
+			a.Articulo,
+			d.Descripcion1,
+			d.Unidad,
+			COALESCE(Demanda90,0)		AS DEMANDA90,
+			COALESCE(Pedido,0)			AS PEDIDO,
+			Disponible,
+			c.Factor
+
+	FROM ArtDisponible			a
+	LEFT JOIN Consumos			b	ON a.Articulo = b.Articulo AND a.Almacen=b.Almacen
+	JOIN ArtUnidad			c	ON a.Articulo = c.Articulo
+	JOIN Art				d	ON a.Articulo = d.Articulo
+   WHERE a.Almacen IN ('10','15')
+     AND c.Unidad = 'CJA-CAJA'
+	 AND Disponible > 0
+)
+
+
+SELECT  ALMACEN,
+		Articulo										AS ARTICULO,
+		Descripcion1									AS DESCRIPCION,
+		Unidad											AS UNIDAD,
+		ROUND(DEMANDA90,4)								AS DEMANDA90,
+		ROUND((DEMANDA90 / 90),4)						AS DEMADIAPROM,
+		ROUND((DEMANDA90 * @FDemandaProm),4)			AS DEMANDAPROM, --variable
+		ROUND(PEDIDO,4)									AS PEDIDO,
+		ROUND(((DEMANDA90 * @FDemandaProm)+PEDIDO),4)	AS MINIMO,
+		ROUND(Disponible,4)								AS EXISTENCIA,
+		ROUND((((DEMANDA90 * @FDemandaProm)+PEDIDO) - Disponible),4)		AS SUGERIDO,
+		Factor											AS PZACAJA,
+		CASE WHEN (((DEMANDA90 * @FDemandaProm)+PEDIDO) - Disponible) < 0.00 THEN 0 ELSE ROUND(((((DEMANDA90 * @FDemandaProm)+PEDIDO) - Disponible) / Factor),4) END AS SUGREDON,
+		@FDemandaProm									AS FACTORDEMANDAPROMEDIO
+		
+  FROM Totales
+ ORDER BY 1,5 DESC
+
+END
+
+
